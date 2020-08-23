3b10d31deb873fd8dc11c0ed2373189895a8cf9d
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-23 15:39:27 +0500

    Removed unnecessary files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const titleElement = getByText(/timer app/i);
-  expect(titleElement).toBeInTheDocument();
-});
