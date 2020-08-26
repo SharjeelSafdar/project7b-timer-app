8511e599687a8f59f8b3ed31ff3f73668654155a
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-26 19:21:16 +0500

    Removed Home and PomodoroSettings components

--- src/components/PomodoroSetting/PomodoroSetting.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import DurationSetting from './PomodoroSetting';
-
-describe ('Tests for <DurationSetting /> Component', () => {
-    it ('Renders Correctly', () => {
-        render(<DurationSetting />);
-    })
-})
