108496d8dfaec9109c579c2129fa8cdf662077d8
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-08-30 21:20:52 +0500

    Added types for props of components

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,9 +2,16 @@
 
 // Type(s) for Reducer State
 type ReducerState = {
+    // minutesSetting: number;
+    // secondsSetting: number;
+    // minutesDisplay: number;
+    // secondsDisplay: number;
     minutes: number;
     seconds: number;
+    // previousMinutes: number;
+    // previousSeconds: number;
     isRunning: boolean;
+    intervalId: NodeJS.Timeout | null;
 }
 
 // Type(s) for Reducer Actions
@@ -28,7 +35,24 @@ type ToggleIsRunning = {
     readonly type: 'TOGGLE_IS_RUNNING';
     readonly payload: null;
 }
-type ReducerActions = IncreaseMinutes | DecreaseMinutes | IncreaseSeconds | DecreaseSeconds | ToggleIsRunning
+type StartTimer = {
+    readonly type: 'START_TIMER';
+    readonly payload: {
+        decreaseSeconds: () => void;
+        resetTimer: () => void;
+        getSecondsLeft: () => number;
+    };
+}
+type StopTimer = {
+    readonly type: 'STOP_TIMER';
+    readonly payload: null;
+}
+type ResetTimer = {
+    readonly type: 'RESET_TIMER';
+    readonly payload: null;
+}
+type ReducerActions = IncreaseMinutes | DecreaseMinutes | IncreaseSeconds | DecreaseSeconds | 
+                      ToggleIsRunning | StartTimer | StopTimer | ResetTimer
 
 // Context Type
 type ContextType = ReducerState & {
@@ -37,4 +61,26 @@ type ContextType = ReducerState & {
     increaseSeconds?: () => void;
     decreaseSeconds?: () => void;
     toggleIsRunning?: () => void;
+    startTimer?: (fn: () => number) => void;
+    stopTimer?: () => void;
+    resetTimer?: () => void;
+    getSeconds?: () => number;
+}
+
+
+type TimerDisplayProps = {
+    minutes: number;
+    seconds: number;
+    setMinutes: React.Dispatch<React.SetStateAction<number>>;
+    setSeconds: React.Dispatch<React.SetStateAction<number>>;
+    isRunning: boolean;
+}
+
+type PlayerProps = {
+    minutes: number;
+    seconds: number;
+    isRunning: boolean;
+    setIsRunning: React.Dispatch<React.SetStateAction<boolean>>;
+    setSeconds: React.Dispatch<React.SetStateAction<number>>;
+    setMinutes: React.Dispatch<React.SetStateAction<number>>;
 }
